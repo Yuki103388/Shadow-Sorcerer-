3387aa5b8353af6d10189547eddb2eb477387f52
DMClone <230381@student.glu.nl>
2025-12-02 16:02:24 +0100

    Code fixes, comments

--- Assets/Developers/Dani/Scripts/Puzzles/WeightPuzzle.cs
+++ Assets/Developers/Dani/Scripts/Puzzles/WeightPuzzle.cs
@@ -21,9 +21,7 @@ public class WeightPuzzle : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Weight"))
-        {
             blocksOnPlate--;
-        }
     }
 
     private void CheckPuzzleCompletion()
@@ -33,7 +31,6 @@ public class WeightPuzzle : MonoBehaviour
             isCompleted = true;
             onPuzzleCompleted.Invoke();
             Debug.Log("Puzzle Completed!");
-            // Add additional logic for puzzle completion here
         }
     }
 }
