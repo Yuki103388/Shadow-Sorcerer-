83b8e4b5dafda6b1472d92d8e8df53b6500ef8ec
DMClone <230381@student.glu.nl>
2025-12-04 11:40:20 +0100

    Duplicatable alternative spawn position

--- Assets/Developers/Dani/Scripts/Duplicatable.cs
+++ Assets/Developers/Dani/Scripts/Duplicatable.cs
@@ -10,6 +10,7 @@ public class Duplicatable : MonoBehaviour
     [SerializeField] private int maxDuplicates;
     [Tooltip("The multiplier of the distance the new object is placed from the original. 2 is next to original")]
     [SerializeField] private float duplicateHeightMultiplier = 2f;
+    [SerializeField] private bool spawnTowardsWand;
     private int currentDuplicates;
     public UnityEvent onDuplicate;
 
@@ -22,9 +23,20 @@ public class Duplicatable : MonoBehaviour
         }
         else if (!originalObject.CanDuplicate()) return;
 
-        // Sets the new object's position above the original based on its collider size
-        Duplicatable Newobject = Instantiate(gameObject, transform.position + (objectCollider.bounds.extents.y * Vector3.up * duplicateHeightMultiplier), transform.rotation).GetComponent<Duplicatable>();
-        onDuplicate.Invoke();
+        Duplicatable Newobject;
+        if (spawnTowardsWand && WandBehaviour.instance != null)
+        {
+            // Sets the new object's position in the direction of the wand from the original based on its collider size
+            Vector3 directionToWand = (WandBehaviour.instance.transform.position - transform.position).normalized;
+            Newobject = Instantiate(gameObject, transform.position + directionToWand * (objectCollider.bounds.extents.magnitude * duplicateHeightMultiplier), transform.rotation).GetComponent<Duplicatable>();
+            Debug.Log("Duplicated at " + Newobject.transform.position);
+        }
+        else
+        {
+            // Sets the new object's position above the original based on its collider size
+            Newobject = Instantiate(gameObject, transform.position + (objectCollider.bounds.extents.y * Vector3.up * duplicateHeightMultiplier), transform.rotation).GetComponent<Duplicatable>();
+        }
+
         if (originalObject == null)
         {
             currentDuplicates++;
@@ -35,6 +47,8 @@ public class Duplicatable : MonoBehaviour
             originalObject.currentDuplicates++;
             Newobject.originalObject = originalObject;
         }
+
+        onDuplicate.Invoke();
     }
 
     public bool CanDuplicate()
