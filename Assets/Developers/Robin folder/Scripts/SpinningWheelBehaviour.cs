bb694823a791a106fac2d02b7c72b9d92174a855
Robi-1224 <230087@student.glu.nl>
2025-12-17 15:36:04 +0100

    rigged wheel test model made, spinning and slowing down working, spinning wheel behaviour created, element wheel created

---
+++ Assets/Developers/Robin folder/Scripts/SpinningWheelBehaviour.cs
@@ -0,0 +1,50 @@
+using Oculus.Interaction.Unity.Input;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Unity.Collections;
+using System.Collections.Generic;
+
+public class SpinningWheelBehaviour : MonoBehaviour
+{
+    private Transform _transform;
+    [SerializeField] private List<WheelElement> _wheelElement;
+    [SerializeField] private float spinSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        _transform = GameObject.Find("wheel").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void SpinWheel(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            StartCoroutine(SpinWheelTest());
+        }
+    }
+
+    private void CheckWinOrLose()
+    {
+
+    }
+
+    private IEnumerator SpinWheelTest()
+    {
+        while(true) { 
+            _transform.Rotate(0, 0, -spinSpeed);
+            spinSpeed = .99f * spinSpeed; 
+            if(spinSpeed <= 0.5f)
+            {
+                spinSpeed = 200;
+                StopAllCoroutines();
+            }
+            yield return null;
+        }
+    }
+}
